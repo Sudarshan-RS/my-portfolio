55914f5fb2046be47157e2c61855991f71ba2c13
Sudarshan-RS <127286541+Sudarshan-RS@users.noreply.github.com>
2023-11-22 16:11:32 +0530

    login

--- src/js/netflix.js
+++ src/js/netflix.js
@@ -1,15 +1,2 @@
-function signInClick() {
-    document.querySelector(".body").style.display = "none";
-    document.querySelector(".faq").style.display = "none";
-    document.querySelector(".tag-line").style.display = "none";
-    document.querySelector(".nav-btn").style.display = "none";
-    document.querySelector(".hero").style.display = "none";
-    document.querySelector(".main-entry-bottom").style.display = "none";
-    document.querySelector(".login").style.display = "flex";
-    document.querySelector(".footer").style.display = "none";
-    document.querySelector("footer").style.marginLeft = "10%";
-    document.querySelector("footer").style.marginRight = "0";
-    document.querySelector(".login-footer").style.display = "grid";
-    document.querySelector(".footer-brand-name").style.display = "none";
-  }
+
   
